66d7510ef02bd10c09b65cb6ad20ae9ce462b6e9
user <geographer29@yandex.ru>
2018-12-05 02:11:49 +0300

    Added Pollard Rho logarithming algorithm

--- src/main/java/ru/geographer29/polytech/labs/logarythm/PollardRhoLogarythm.java
+++ src/main/java/ru/geographer29/polytech/labs/logarythm/PollardRhoLogarythm.java
@@ -1,18 +1,10 @@
 package ru.geographer29.polytech.labs.logarythm;
 
 import java.math.BigInteger;
-import java.util.LinkedList;
-import java.util.List;
 
 public class PollardRhoLogarythm extends AbsractLogarythm {
-    long launchTime = System.currentTimeMillis();
-    long stopLimit = 3600000; // 1 hour
-    //List<BigInteger> cs = new LinkedList<>();
-    //List<BigInteger> ds = new LinkedList<>();
-    BigInteger[] cs = new BigInteger[10];
-    BigInteger[] ds = new BigInteger[10];
-    BigInteger[] cLogs = new BigInteger[10];
-    BigInteger[] dLogs = new BigInteger[10];
+    private long launchTime = System.currentTimeMillis();
+    private BigInteger five = BigInteger.valueOf(5);
 
     public PollardRhoLogarythm(BigInteger a, BigInteger b, BigInteger p, BigInteger q) {
         super(a, b, p, q);
@@ -20,71 +12,59 @@ public class PollardRhoLogarythm extends AbsractLogarythm {
 
     @Override
     public String call() throws Exception {
-        BigInteger r = rhoLogorythm();
-        print();
-        if (r.compareTo(one) == 0)
-            sb.append("Logarythm has not computed, planned termination\n");
-        if (r.compareTo(minusOne) == 0)
-            sb.append("Logarythm has not found\n");
+        printHeader();
         return sb.toString();
     }
 
     private BigInteger f(BigInteger x) {
-        return x.multiply(x).add(one);
+        if (x.compareTo(p.shiftRight(1)) == -1)
+            return x.multiply(a).mod(p);
+        else
+            return x.multiply(b).mod(p);
     }
 
-    private BigInteger rhoLogorythm() {
+    private BigInteger rhoLogarythm() {
         BigInteger
                 u = two,
                 v = two,
-                c = a.modPow(u, p).multiply(b.modPow(v, p)),  //check it!
-                d = c;
+                c, d;
+
+        c = a.pow(u.intValue()).multiply(b.pow(v.intValue()));
+        c = c.mod(p);
+        d = c;
+
+        //System.out.println(" a = " + a + " b = " + b);
+        //System.out.println(" p = " + p + " q = " + r);
+        //System.out.println(" c = " + c + " d = " + d + "\n");
 
         for (;;) {
-            if (System.currentTimeMillis() - launchTime > 3600000 ||
-            c.compareTo(d) != 0)
-                break;
+            saveLine(c, d);
 
             c = f(c).mod(p);
             d = f(f(d)).mod(p);
 
-            if (iteration < 5) {
-                cs[iteration] = c;
-                ds[iteration] = d;
-                cLogs[iteration] = c;
-                dLogs[iteration] = d;
-            } else {
-                int pos = (iteration % 5) + 5;
-                cs[pos] = c;
-                ds[pos] = d;
-                cLogs[pos] = c;
-                dLogs[pos] = d;
-            }
-
-            System.out.println(" C=" + c + " D=" + d);
-
+            //System.out.println(" C = " + c + " D = " + d);
 
             iteration++;
+
+            if (System.currentTimeMillis() - launchTime > 3600000 ||
+                    c.subtract(d).mod(p).equals(zero))
+                break;
         }
 
         return one;
     }
 
-    private void print() {
+    private void printHeader() {
         sb.append("Computation using ").append(getClass().getSimpleName())
                 .append("\nCompression function = x*x + 5\n");
 
-        for (int i = 1; i < 10; i++) {
-            if (cs[i] == null)
-                break;
+        BigInteger r = rhoLogarythm();
 
-            sb
-                    .append("C = ")
-                    .append(cs[i])
-                    .append(" D = ")
-                    .append(ds[i])
-                    .append("\n");
-        }
+        if (r.compareTo(one) == 0)
+            sb.append("Logarythm has not computed, planned termination\n");
+        if (r.compareTo(minusOne) == 0)
+            sb.append("Logarythm has not found\n");
 
         sb
                 .append("Total iterations = ")
@@ -92,6 +72,15 @@ public class PollardRhoLogarythm extends AbsractLogarythm {
                 .append("\nComputation time = ")
                 .append(System.currentTimeMillis() - launchTime)
                 .append(" ms\n");
+    }
+
+    private void saveLine(BigInteger c, BigInteger d) {
+        sb
+                .append(" C = ")
+                .append(c)
+                .append(" D = ")
+                .append(d)
+                .append("\n");
     }
 
 
